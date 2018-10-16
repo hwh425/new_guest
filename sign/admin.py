7070723f660b3bf99929aa5ae8d01e9246bf5c56
hwh425 <840160278@qq.com>
2018-10-16 17:30:55 +0800

    add model

--- sign/admin.py
+++ sign/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from sign.models import Event, Guest
+
 
 # Register your models here.
+class EventAdmin(admin.ModelAdmin):
+    # 显示列表
+    list_display = ['id', 'name', 'status', 'address', 'start_time']
+    # 搜索栏
+    search_fields = ['name']
+    # 过滤器
+    list_filter = ['status']
+
+
+class GuestAdmin(admin.ModelAdmin):
+    list_display = ['real_name', 'phone', 'sign', 'create_time', 'event']
+    # 搜索栏
+    search_fields = ['real_name', 'phone']
+    # 过滤器
+    list_filter = ['sign']
+
+
+admin.site.register(Event, EventAdmin)
+admin.site.register(Guest, GuestAdmin)
