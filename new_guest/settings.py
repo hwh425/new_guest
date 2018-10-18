a423f625f568ced2a0877278e128a41d1efa69de
hwh425 <840160278@qq.com>
2018-10-18 17:34:25 +0800

    django test

--- new_guest/settings.py
+++ new_guest/settings.py
@@ -122,9 +122,10 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
+# 界面中文显示
+LANGUAGE_CODE = 'zh-hans'
+# LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
